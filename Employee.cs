625e8225742dd248927a963e00863e16b6b94a17
Niteesh-Agadi <78499704+Niteesh-Agadi@users.noreply.github.com>
2021-03-31 18:03:47 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emp_Details
+{
+    class Employee
+    {
+        public string name;
+        public string Designation;
+        private int salary;
+
+        public int GetSalary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+
+        public void Employee_Details()
+        {
+            Console.WriteLine("Enter the Name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter the Designation:");
+            Designation = Console.ReadLine();
+            Console.WriteLine("Enter the Salary:");
+            salary = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void Display()
+        {
+            Employee_Details();
+        }
+        public void Display_Info()
+        {
+            Console.WriteLine("name: {0}    ,designation :{1} ,salary :{2} ", name, Designation, salary);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Emp_Details
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] EId = new Employee[3];
+            EId[0] = new Employee();
+            EId[1] = new Employee();
+            EId[2] = new Employee();
+
+            EId[0].Display();
+            EId[1].Display();
+            EId[2].Display();
+
+            EId[0].Display_Info();
+            EId[1].Display_Info();
+            EId[2].Display_Info();
+
+        }
+    }
+}
